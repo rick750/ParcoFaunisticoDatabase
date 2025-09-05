6802c5cd4df88bcd491a5018093d0cec261d0bd0
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-09-05 17:44:33 +0200

    Add gradle rows to create jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
-    java
-    application
+    id 'java'
+    id 'application'
+    id 'com.github.johnrengelman.shadow' version '8.1.1'
 }
+
+group = 'it.parcofaunistico'
+version = '1.0.0'
 
 repositories {
     mavenCentral()
@@ -9,17 +13,41 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21)) // o 22 se vuoi usare la tua versione
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
 application {
-    // la classe con il main
     mainClass.set("parcofaunistico.App")
 }
 
 dependencies {
-    // esempio: driver mysql
-    // implementation("mysql:mysql-connector-java:8.1.0")
     implementation("com.mysql:mysql-connector-j:8.3.0")
+}
+
+jar {
+    manifest {
+        attributes(
+            'Implementation-Title': project.name,
+            'Implementation-Version': project.version,
+            'Main-Class': application.mainClass.get()
+        )
+    }
+}
+
+shadowJar {
+    archiveBaseName.set(project.name)
+    archiveClassifier.set('') 
+    archiveVersion.set(project.version.toString())
+    manifest {
+        attributes 'Main-Class': application.mainClass.get()
+    }
+    mergeServiceFiles() 
+}
+
+// Optional: alias task per chiarezza
+tasks.register('fatJar') {
+    dependsOn tasks.named('shadowJar')
+    group = 'build'
+    description = 'Genera un JAR eseguibile contenente tutte le dipendenze (uber-jar).'
 }
